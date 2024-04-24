fa0f17420e36486c95538df0aee022496977d5f4
Mohamed Asaker <asakermohamad471@icloud.com>
2024-04-24 16:01:20 -0700

    Fixed SLO creation script (#19)
    * Fixed SLO creation script
    
    * updated cleanup script

--- scripts/eks/appsignals/one-step/cleanup.sh
+++ scripts/eks/appsignals/one-step/cleanup.sh
@@ -46,6 +46,10 @@ echo "Deleting canaries"
 ../create-canaries.sh $REGION delete
 check_if_step_failed_and_exit "There was an error deleting the canaries. Please make sure they are deleted properly before proceeding with the following steps"
 
+# force delete db service and pvc as this might cause the cluster delete to hang
+kubectl delete pods -l io.kompose.service=db
+kubectl delete pvc data  --grace-period=0 --force 
+
 ../deploy-sample-app.sh $CLUSTER_NAME $REGION $NAMESPACE delete
 check_if_step_failed_and_exit "There was an error deleting the sample apps. Please make sure they are deleted properly before proceeding with the following steps"
 
@@ -65,4 +69,7 @@ check_if_step_failed "There was an error deleting the kinesis stream."
 
 # remove DDB table
 aws dynamodb delete-table --table-name apm_test --region $REGION
+check_if_step_failed "There was an error deleting the dynamodb table."
+
+aws dynamodb delete-table --table-name BillingInfo --region $REGION
 check_if_step_failed "There was an error deleting the dynamodb table."
