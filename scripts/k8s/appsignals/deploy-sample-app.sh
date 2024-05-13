9d44dd364aa69b4125bd02dd8c5756b576bf5d91
Ping Xiang <64551395+pxaws@users.noreply.github.com>
2024-05-13 16:59:29 -0700

    set up k8s demo (#29)

--- scripts/k8s/appsignals/deploy-sample-app.sh
+++ scripts/k8s/appsignals/deploy-sample-app.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -x
 
-alias kubectl="minikube kubectl --" 
-
-kubectl() {
- minikube kubectl -- "$@"
-}
-
-kubectl get pod
-
 # change the directory to the script location so that the relative path can work
 cd "$(dirname "$0")"
 
@@ -17,16 +9,21 @@ REGION=$1
 
 
 ACCOUNT=$(aws sts get-caller-identity | jq -r '.Account')
+NAMESPACE=default
 
+kubectl apply -f ./sample-app/db/storageclass.yaml 
 kubectl apply -f ./sample-app/db/db-pvc.yaml 
-kubectl apply -f ./sample-app/db/
+kubectl apply -f ./sample-app/db/db-deployment.yaml
+kubectl apply -f ./sample-app/db/db-service.yaml
 host=db.$NAMESPACE.svc.cluster.local
 
 sleep 60
 
+repo_prefix=$(aws ecr-public describe-repositories --repository-names traffic-generator --region us-east-1 --query 'repositories[0].repositoryUri' --output text | cut -d'/' -f1,2)
+
 for config in $(ls ./sample-app/*.yaml)
 do
-    sed -e "s/111122223333.dkr.ecr.us-west-2/$ACCOUNT.dkr.ecr.$REGION/g" -e 's#\${REGION}'"#${REGION}#g" -e 's#\${DB_SERVICE_HOST}'"#${host}#g" $config | kubectl apply -f -
+    sed -e "s#111122223333.dkr.ecr.us-west-2.amazonaws.com#${repo_prefix}#g" -e 's#\${REGION}'"#${REGION}#g" -e 's#\${DB_SERVICE_HOST}'"#${host}#g" $config | kubectl apply -f -
 done
 
 # Apply k8s nginx configuration
@@ -39,7 +36,7 @@ sleep 60
 endpoint=$(kubectl get svc -n ingress-nginx | grep "ingress-nginx" | awk '{print $3}')
 
 # Start the traffic generator
-sed -e "s/111122223333.dkr.ecr.us-west-2/$ACCOUNT.dkr.ecr.$REGION/g" -e "s/SAMPLE_APP_END_POINT/${endpoint}/g"  ./traffic-generator.yaml | kubectl apply -f -
+sed -e "s#111122223333.dkr.ecr.us-west-2.amazonaws.com#${repo_prefix}#g" -e "s/SAMPLE_APP_END_POINT/${endpoint}/g"  ./traffic-generator.yaml | kubectl apply -f -
 
 # Print the endpoint
 echo "Started the traffic generator to send traffic to http://${endpoint}"
