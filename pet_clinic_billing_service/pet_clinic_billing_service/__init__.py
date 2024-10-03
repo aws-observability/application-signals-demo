3ed084d8bb1f661f547a957fb16a0acdd3aacc1e
zzhlogin <zzhlogin@amazon.com>
2024-10-03 18:46:49 -0400

    Ecs platform support (#59)
    *Description of changes:*
    This PR add ECS platform support for the demo app for all Java and
    Python services.
    
    For the Java and python applications, right now, each service register
    with eureka using there own IP address. However, the IP won't be
    reachable in ECS services, we apply patches for service configuration
    files, then use service discovery namespace instead of instance IP.
    
    Test Service Map from console:
     
    ![Screenshot 2024-09-27 at 1 39
    56 PM](https://github.com/user-attachments/assets/bd94974b-de3f-4944-8b0b-0437136e4afd)
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- pet_clinic_billing_service/pet_clinic_billing_service/__init__.py
+++ pet_clinic_billing_service/pet_clinic_billing_service/__init__.py
@@ -67,12 +67,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 local_ip = s.getsockname()[0]
 s.close()
-
+billing_service_ip = os.environ.get('BILLING_SERVICE_IP', local_ip)
 eureka_server_url = os.environ.get('EUREKA_SERVER_URL', 'localhost')
 # Register with Eureka
 eureka_client.init(
     eureka_server=f"http://{eureka_server_url}:8761/eureka",
-    instance_host=local_ip,
+    instance_host=billing_service_ip,
     app_name="billing-service",
     instance_port=8800,  # Django's default port
     # ... other configuration options
--- pet_clinic_insurance_service/pet_clinic_insurance_service/__init__.py
+++ pet_clinic_insurance_service/pet_clinic_insurance_service/__init__.py
@@ -7,12 +7,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 local_ip = s.getsockname()[0]
 s.close()
+insurance_service_ip = os.environ.get('INSURANCE_SERVICE_IP', local_ip)
 
 eureka_server_url = os.environ.get('EUREKA_SERVER_URL', 'localhost')
 # Register with Eureka
 eureka_client.init(
     eureka_server=f"http://{eureka_server_url}:8761/eureka",
-    instance_host=local_ip,
+    instance_host=insurance_service_ip,
     app_name="insurance-service",
     instance_port=8000,  # Django's default port
     # ... other configuration options
