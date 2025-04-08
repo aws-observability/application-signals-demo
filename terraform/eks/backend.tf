152a8a79472f34a44758047f741461ba160a36cc
Yong Tao <taoyong@amazon.com>
2025-04-07 19:50:07 -0700

    update backend config

--- terraform/eks/backend.tf
+++ terraform/eks/backend.tf
@@ -1,5 +1,7 @@
-backend "s3" {
-  bucket = "dummy"
-  key    = "dummy"
-  region = "us-east-1"
+terraform {
+    backend "s3" {
+        bucket = "dummy"
+        key    = "dummy"
+        region = "us-east-1"
+    }
 }
